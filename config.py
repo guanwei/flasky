a1db11b5253f6bd9462db4c0e6df1a6954798fc2
Edward Guan <285006386@qq.com>
2018-08-23 14:46:59 +0800

    update

--- config.py
+++ config.py
@@ -4,10 +4,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true')
-    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', 'false')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.exmail.qq.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', 465))
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', 'true')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
